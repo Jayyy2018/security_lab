cf742925bc2c0ed3a04ce5cdc8f50e8f9e6c7f8c
Your Name Hereyzhang559 <yzhan559@hku.hk>
2019-04-16 19:53:53 -0400

    fix server

--- benefits.js
+++ benefits.js
@@ -48,10 +48,12 @@ function displayBenefits1(req,res,next,succ,err,data)
    for (var i = 0; i < data.rows.length; ++i) {
       if (data != null){
        var date = data.rows[i].benefitStartDate;
+       if (date != null){
        var mon = date.getMonth() + 1;
        if (mon < 10) mon = "0"+mon;
        var string = date.getFullYear() + "-" + mon + "-" + date.getDate();
        console.log("CONVERT",date,string);
+     }
        data.rows[i].benefitStartDate = string;
      }
     }
