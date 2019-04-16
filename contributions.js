8cca49a9dac4083b2a1679d518d95cab22df34f9
Your Name Hereyzhang559 <yzhan559@hku.hk>
2019-04-16 16:22:04 -0400

    fix eval

--- contributions.js
+++ contributions.js
@@ -62,9 +62,10 @@ function displayContributions1(req,res,next,sts,err,data)
 function handleContributionsUpdate(req,res,next)
 {
    // convert to numbers
-   var preTax = eval(req.body.preTax);
-   var afterTax = eval(req.body.afterTax);
-   var roth = eval(req.body.roth);
+   // fix 2, change eval() to validator.toFloat()
+   var preTax = validator.toFloat(req.body.preTax);
+   var afterTax = validator.toFloat(req.body.afterTax);
+   var roth = validator.toFloat(req.body.roth);
 
    var userId = req.session.userId;
 
