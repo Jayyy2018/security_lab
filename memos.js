f35cce07784106b31278a59cd1c42b968bf6e095
Your Name Hereyzhang559 <yzhan559@hku.hk>
2019-04-16 23:03:15 -0400

    memo

--- memos.js
+++ memos.js
@@ -22,7 +22,7 @@ function addMemos(req,res,next)
    // fix 1
    // var q = "INSERT INTO Memos(memo) VALUES ( '" + memo + "' )";
    // db.query(q,function(e1,d1) { addMemos1(req,res,next,e1,d1); });
-
+   var memo = req.body.memo;
    var q = "INSERT INTO Memos(memo) VALUES ( ? )";
    db.query(q, [memo], function(e1,d1) { addMemos1(req,res,next,e1,d1); });
    
